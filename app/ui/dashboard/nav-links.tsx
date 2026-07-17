dd08c2c736e697cd1c1d5be33ab9254521dbe428
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-07-17 09:00:48 +0700

    modify tampilan NavLinks Dan button sign out  agar selebar sidenav panda tampilan desktop.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -33,7 +33,7 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-auto grow flex-col items-center justify-center gap-1 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-row md:h-[48px] md:flex-none md:justify-start md:gap-2 md:p-2 md:px-3",
+              "flex h-auto flex-col items-center justify-center gap-1 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-row md:h-11 md:w-full md:justify-start md:gap-2 md:p-2 md:px-3",
               {
                 "bg-sky-100 text-blue-600": pathname === link.href,
               }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import IlaundryLogo from '@/app/ui/ilaundry-logo';
 
 export default function SideNav() {
   return (
@@ -10,8 +8,8 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between items-center space-x-2 md:flex-col md:space-x-0 md:space-y-2 md:items-start">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-auto w-full grow flex-col items-center justify-center gap-1 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-row md:h-[48px] md:flex-none md:justify-start md:gap-2 md:p-2 md:px-3">
+        <form className="w-full">
+          <button className="flex h-auto w-full flex-col items-center justify-center gap-1 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-row md:h-11 md:w-full md:justify-start md:gap-2 md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="block text-[10px] md:text-sm">Sign Out</div>
           </button>
