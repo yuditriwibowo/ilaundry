a11e9175325e72fe580dca7493e6e2403c00ade0
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-07-18 03:33:17 +0700

    perbaikan sidenav dan navlink untuk mobile dan desktop.

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -4,17 +4,19 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="flex grow flex-row justify-between items-center space-x-2 md:flex-col md:space-x-0 md:space-y-2 md:items-start">
-        <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form className="w-full">
-          <button className="flex h-auto w-full flex-col items-center justify-center gap-1 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-row md:h-11 md:w-full md:justify-start md:gap-2 md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="block text-[10px] md:text-sm">Sign Out</div>
-          </button>
-        </form>
+      <div className="flex h-full flex-col px-3 py-4 md:px-2">
+        <div className="flex flex-row justify-between items-center space-x-2 md:flex-col md:space-x-0 md:space-y-2 md:items-start">
+          <div className="grow flex flex-row justify-between items-center gap-2 md:contents">
+            <NavLinks />
+            <form className="grow md:w-full">
+              <button className="flex h-auto w-full grow flex-col items-center justify-center gap-1 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-row md:h-auto md:w-full md:justify-start md:gap-2 md:p-2 md:px-3">
+                <PowerIcon className="w-6" />
+                <div className="block text-[10px] md:text-sm">Sign Out</div>
+              </button>
+            </form>
+          </div>
+          <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
