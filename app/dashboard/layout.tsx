efdc76fff70d6c472aa8cde70af1e5d3522bf0a8
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-07-18 23:40:14 +0700

    SideNav pada tampilan mobile fixed di bawah, content scrolling

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,13 +3,13 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     /* h-full memastikan layout dashboard mengisi seluruh sisa ruang dari RootLayout */
-    <div className="flex h-full flex-col md:flex-row md:overflow-hidden">
+    <div className="flex h-full flex-col md:flex-row overflow-hidden">
       <div className="hidden md:block w-full flex-none md:w-64">
         <SideNav />
       </div>
       
       {/* grow membuat konten mengisi ruang tengah, p-6 memberi jarak agar tidak tertutup navigasi bawah */}
-      <div className="grow p-6 md:overflow-y-auto md:p-12 pb-24 md:pb-12">
+      <div className="grow p-6 overflow-y-auto md:p-12 pb-24 md:pb-12">
         {children}
       </div>
       
