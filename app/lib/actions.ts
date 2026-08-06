3c6c1fa97e0302b24610675e9df6fc6cec83f882
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-08-06 08:12:20 +0700

    Create Pelanggan done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,14 +1,54 @@
 "use server";
 
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  date: z.string(),
+  status: z.enum(["pending", "paid"]),
+});
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
-  };
+  });
 
-  console.log(rawFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  console.log({ customerId, amountInCents, status, date });
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath("/dashboard/pesanan");
+  redirect("/dashboard/pesanan");
 }
+
+const PelangganSchema = z.object({
+  id: z.string(),
+  nama: z.string(),
+  no_hp: z.string(),
+  alamat: z.string(),
+  email: z.string(),
+  image_url: z.string(),
+  tgl_daftar: z.string(),
+});
+const CreatePelanggan = PelangganSchema.omit({
+  id: true,
+  image_url: true,
+  tgl_daftar: true,
+});
 
 export async function createPelanggan(formData: FormData) {
   const rawFormData = {
@@ -17,7 +57,15 @@ export async function createPelanggan(formData: FormData) {
     email: formData.get("email"),
     alamat: formData.get("alamat"),
   };
+  const { nama, no_hp, email, alamat } = CreatePelanggan.parse(rawFormData);
+  const tgl_daftar = new Date().toISOString().split("T")[0];
+  const image_url = "/pelanggan/avatar.png";
 
-  console.log(rawFormData);
+  console.log({ nama, no_hp, email, alamat, tgl_daftar, image_url });
+  await sql`
+  INSERT INTO pelanggan (nama, no_hp, email, alamat, tgl_daftar, image_url)
+  VALUES (${nama}, ${no_hp}, ${email}, ${alamat}, ${tgl_daftar}, ${image_url})
+  `;
+  revalidatePath("/dashboard/pelanggan");
+  redirect("/dashboard/pelanggan");
 }
-
