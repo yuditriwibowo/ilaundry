421f5df85485a154e67f726ca29a392b2a6b845a
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-08-14 23:40:51 +0700

    fixing header mobile-portrait

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -6,15 +6,11 @@ import YlaundryLogo from "./ylaundry-logo";
 
 export default function Header() {
   const pathname = usePathname();
-  const showGapOnMobile = pathname === "/" || pathname === "/laundry";
-  const hideOnMobile = [
-    "/laundry/laporan",
-    "/laundry/pelanggan",
-    "/laundry/pesanan",
-  ].includes(pathname);
+  const showOnMobile = ["/", "/laundry"].includes(pathname);
+  const showGapOnMobile = showOnMobile;
 
   return (
-    <header className={`flex-none ${showGapOnMobile ? "pt-3" : "pt-0 md:pt-3"} px-4 pb-2 z-50 flex justify-center ${hideOnMobile ? "hidden md:flex" : ""}`}>
+    <header className={`flex-none ${showGapOnMobile ? "pt-3" : "pt-0 md:pt-3"} px-4 pb-2 z-50 flex justify-center ${!showOnMobile ? "hidden md:flex" : ""}`}>
       <Link
         className="flex h-16 items-center justify-start rounded-xl bg-blue-600 p-4 md:h-20 w-full md:w-95/100 shadow-md"
         href="/"
