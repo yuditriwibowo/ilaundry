5b90cccfef515109a9fa797ff888b473a22d31ce
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-08-15 00:19:11 +0700

    kurangi tinggi header

--- app/laundry/pelanggan/create/page.tsx
+++ app/laundry/pelanggan/create/page.tsx
@@ -4,7 +4,7 @@ import Breadcrumbs from "@/app/ui/breadcrumbs";
 export default async function Page() {
   return (
     <div>
-        <div className="bg-blue-200 px-4 -mx-4 rounded-b-xl flex items-center min-h-[112px] md:bg-gray-50 md:pb-0 md:px-0 md:pt-0 md:mx-0 md:rounded-b-none md:min-h-0">
+        <div className="bg-blue-200 px-4 -mx-4 rounded-b-xl flex items-center min-h-[90px] md:bg-gray-50 md:pb-0 md:px-0 md:pt-0 md:mx-0 md:rounded-b-none md:min-h-0">
          <Breadcrumbs
            breadcrumbs={[
               { label: "Pelanggan", href: "/laundry/pelanggan" },
