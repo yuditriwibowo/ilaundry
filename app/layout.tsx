330c61ee559965c52320bb2a5518cfb1db8dfbbe
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-08-16 05:56:52 +0700

    warna status bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#3b82f6",
+  themeColor: "#60a5fa",
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
