08ee6ccbe9d05ca17204a8e6dc5c5589b9a8c732
Yudi Tri Wibowo <yuditriwibowo@gmail.com>
2026-09-26 18:56:29 +0700

    sticky header pengaturan iklan

--- app/laundry/pengaturan/info-iklan/create/page.tsx
+++ app/laundry/pengaturan/info-iklan/create/page.tsx
@@ -17,7 +17,7 @@ export default async function Page() {
 
   return (
     <div>
-      <div className="bg-gradient-to-r from-blue-700 via-blue-600 to-indigo-700 shadow-md px-4 -mx-4 rounded-b-xl flex items-center min-h-[90px] md:bg-none md:bg-gray-50 md:pb-0 md:px-0 md:pt-0 md:mx-0 md:rounded-b-none md:min-h-0">
+      <div className="sticky top-0 z-10 bg-gradient-to-r from-blue-700 via-blue-600 to-indigo-700 shadow-md px-4 -mx-4 rounded-b-xl flex items-center min-h-[90px] md:static md:bg-none md:bg-gray-50 md:pb-0 md:px-0 md:pt-0 md:mx-0 md:rounded-b-none md:min-h-0">
         <Breadcrumbs
           breadcrumbs={[
             { label: "Pengaturan Info & Iklan", href: "/laundry/pengaturan/info-iklan" },
